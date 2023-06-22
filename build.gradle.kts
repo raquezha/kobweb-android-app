6d428a58ab9ced8c6dd8457324908ffe2e965ae8
raquezha <jan@nueca.com.ph>
2023-06-22 16:26:00 +0800

    Add android-app module

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,27 @@
+// 3 remove buildscript in favor of plugins
+// https://plugins.gradle.org/plugin/org.jetbrains.kotlin.android
+//buildscript {
+//    dependencies {
+//        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.21")
+//    }
+//}
+
+
+
+plugins {
+     // for android app
+     // https://mvnrepository.com/artifact/com.android.application/com.android.application.gradle.plugin?repo=google
+     alias(libs.plugins.androidApplication) apply false
+     alias(libs.plugins.kotlinAndroid) apply false
+     alias(libs.plugins.kotlin.multiplatform) apply false
+
+}
+
 subprojects {
-    repositories {
-        mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-        google()
-        maven("https://us-central1-maven.pkg.dev/varabyte-repos/public")
-    }
+     repositories {
+          mavenCentral()
+          maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+          google()
+          maven("https://us-central1-maven.pkg.dev/varabyte-repos/public")
+     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,16 @@
 pluginManagement {
     repositories {
+        // 1. Add this 2 dependencies
+        google()
+        mavenCentral()
         gradlePluginPortal()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         maven("https://us-central1-maven.pkg.dev/varabyte-repos/public")
     }
 }
 
+
 rootProject.name = "app"
 
 include(":site")
+include(":app-android")
